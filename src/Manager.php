4fbae33f7bc52de6d95ac51946e4578a09d3f676
Dennis Birkholz <ecryptfs@birkholz.org>
2017-05-27 14:56:31 +0200

    More key management.

--- src/Manager.php
+++ src/Manager.php
@@ -10,6 +10,45 @@
 namespace Iqb\Ecryptfs;
 
 /**
+ * Number of bytes to use from the supplied salt.
+ */
+const ECRYPTFS_SALT_SIZE = 8;
+
+/**
+ * The salt used for creating the file encryption key encryption key from the passphrase, unhex to use it.
+ */
+const ECRYPTFS_DEFAULT_SALT_HEX = "0011223344556677";
+
+/**
+ * The salt used for creating the file name encryption key from the passphrase.
+ * Due to a programming error in the original ecryptfs user space library,
+ * the salt is not unhexed before use. As a result only the first half is used literally.
+ * 
+ * See ECRYPTFS_DEFAULT_SALT_FNEK for the real salt used.
+ */
+const ECRYPTFS_DEFAULT_SALT_FNEK_HEX = "9988776655443322";
+
+/**
+ * The salt used for creating the file name encryption key from the passphrase.
+ */
+const ECRYPTFS_DEFAULT_SALT_FNEK = "99887766";
+
+/**
+ * Number of iterations when deriving the keys from the passphrase
+ */
+const ECRYPTFS_DEFAULT_NUM_HASH_ITERATIONS = 65536;
+
+/**
+ * Number of raw bytes used from signature hash
+ */
+const ECRYPTFS_SIG_SIZE = 8;
+
+/**
+ * Algorith used to generate keys from the supplied passphrase
+ */
+const ECRYPTFS_KEY_DERIVATION_ALGO = "sha512";
+
+/**
  * This is the entry point for all EcryptFS operations.
  * All keys must be added here.
  *
@@ -18,48 +57,21 @@ namespace Iqb\Ecryptfs;
 class Manager
 {
     /**
-     * Number of bytes to use from the supplied salt.
-     */
-    const ECRYPTFS_SALT_SIZE = 8;
-    
-    /**
-     * The salt used for creating the data (header) encryption key from the passphrase, unhex to use it.
-     */
-    const ECRYPTFS_DEFAULT_SALT_HEX = "0011223344556677";
-    
-    /**
-     * The salt used for creating the file name encryption key from the passphrase.
-     * Due to a programming error in the original ecryptfs user space library,
-     * the salt is not unhexed before use. As a result only the first half is used literally.
-     * 
-     * See ECRYPTFS_DEFAULT_SALT_FNEK for the real salt used.
-     */
-    const ECRYPTFS_DEFAULT_SALT_FNEK_HEX = "9988776655443322";
-    
-    /**
-     * The salt used for creating the file name encryption key from the passphrase.
-     */
-    const ECRYPTFS_DEFAULT_SALT_FNEK = "99887766";
-    
-    /**
-     * Number of iterations when deriving the keys from the passphrase
-     */
-    const ECRYPTFS_DEFAULT_NUM_HASH_ITERATIONS = 65536;
-    
-    /**
-     * Number of raw bytes used from signature hash
-     */
-    const ECRYPTFS_SIG_SIZE = 8;
-    
-    /**
-     * Algorith used to generate keys from the supplied passphrase
-     */
-    const ECRYPTFS_KEY_DERIVATION_ALGO = "sha512";
-    
-    /**
-     * Mapping from (hex) signature to actuall key
+     * Mapping from (hex) signature to actual key
      */
     private $keys = [];
+    
+    /**
+     * Signature of the default file name encryption key
+     * @var string
+     */
+    private $defaultFNEK;
+    
+    /**
+     * Signature of the default file encryption key encryption key
+     * @var string
+     */
+    private $defaultFEKEK;
     
     
     /**
@@ -71,13 +83,13 @@ class Manager
      */
     public function addKey(string $passphrase, string $salt) : string
     {
-        $key = \hash(self::ECRYPTFS_KEY_DERIVATION_ALGO, \substr($salt, 0, self::ECRYPTFS_SALT_SIZE) . $passphrase, true);
+        $key = \hash(ECRYPTFS_KEY_DERIVATION_ALGO, \substr($salt, 0, ECRYPTFS_SALT_SIZE) . $passphrase, true);
         
-        for ($i=1; $i<self::ECRYPTFS_DEFAULT_NUM_HASH_ITERATIONS; $i++) {
-            $key = \hash(self::ECRYPTFS_KEY_DERIVATION_ALGO, $key, true);
+        for ($i=1; $i<ECRYPTFS_DEFAULT_NUM_HASH_ITERATIONS; $i++) {
+            $key = \hash(ECRYPTFS_KEY_DERIVATION_ALGO, $key, true);
         }
         
-        $signature = \substr(\hash(self::ECRYPTFS_KEY_DERIVATION_ALGO, $key, false), 0, self::ECRYPTFS_SIG_SIZE*2);
+        $signature = \substr(\hash(ECRYPTFS_KEY_DERIVATION_ALGO, $key, false), 0, ECRYPTFS_SIG_SIZE*2);
         $this->keys[$signature] = \bin2hex($key);
         
         return $signature;
@@ -94,13 +106,51 @@ class Manager
     
     
     /**
+     * Get the key for the supplied signature
+     */
+    public function getKey(string $signature) : string
+    {
+        if (!isset($this->keys[$signature])) {
+            throw new \InvalidArgumentException("No key with signature $signature exists!");
+        }
+        
+        return $this->keys[$signature];
+    }
+    
+    
+    /**
+     * Get the signature of the default file name encryption key
+     */
+    public function getDefaultFNEK() : string
+    {
+        return $this->defaultFNEK;
+    }
+    
+    
+    /**
+     * Get the signature of the default file encryption key encryption key
+     */
+    public function getDefaultFEKEK() : string
+    {
+        return $this->defaultFEKEK;
+    }
+    
+    
+    /**
      * Use the supplied passphrase to generate and register keys data and file name encryption keys from it.
      * 
      * @param string $passphrase
      */
     public function usePassphrase(string $passphrase)
     {
-        $this->addKey($passphrase, self::ECRYPTFS_DEFAULT_SALT_FNEK);
-        $this->addKey($passphrase, \hex2bin(self::ECRYPTFS_DEFAULT_SALT_HEX));
+        $fnek = $this->addKey($passphrase, ECRYPTFS_DEFAULT_SALT_FNEK);
+        if (!$this->defaultFNEK) {
+            $this->defaultFNEK = $fnek;
+        }
+        
+        $fekek = $this->addKey($passphrase, \hex2bin(ECRYPTFS_DEFAULT_SALT_HEX));
+        if (!$this->defaultFEKEK) {
+            $this->defaultFEKEK = $fekek;
+        }
     }
 }
