51fb3ddb0ce6709be0cba7b166697f1f42663a33
Dennis Birkholz <ecryptfs@birkholz.org>
2017-10-13 02:02:32 +0200

    Moved a lot of method into Util class. Finalized Tag70Packet and made Tag class obsolete.

--- src/Tag11Packet.php
+++ src/Tag11Packet.php
@@ -11,45 +11,45 @@ namespace Iqb\Ecryptfs;
 
 /**
  * Literal Data Packet (Tag 11)
- * 
+ *
  * @author Dennis Birkholz <ecryptfs@birkholz.org>
  * @link https://tools.ietf.org/html/rfc2440#section-5.9 OpenPGP Message Format: Literal Data Packet (Tag 11)
  * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/keystore.c?h=v4.11.3#n1536 parse_tag_11_packet
  * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/keystore.c?h=v4.11.3#n2123 write_tag_11_packet
  */
-final class Tag11Packet extends Tag
+final class Tag11Packet
 {
     /**
      * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/ecryptfs_kernel.h?h=v4.11.3#n141
      */
     const PACKET_TYPE = 0xED;
-    
+
     const MIN_PACKET_LENGTH = 14;
-    
+
     /**
-     * Binary data format specifier, see keystore.c  
+     * Binary data format specifier, see keystore.c
      * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/keystore.c?h=v4.11.3#n1611
      */
     const FORMAT_SPECIFIER = 0x62;
-    
+
     /**
      * Hardcoded filename field
      * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/keystore.c?h=v4.11.3#n2148
      */
     const PACKET_FILENAME = '_CONSOLE';
-    
+
     /**
      * @var string
      */
     public $contents;
-    
-    
+
+
     public function __construct($contents)
     {
         $this->contents = $contents;
     }
-    
-    
+
+
     /**
      * Generate the binary representation of this packet
      */
@@ -65,8 +65,8 @@ final class Tag11Packet extends Tag
             . $this->contents
         ;
     }
-    
-    
+
+
     /**
      * Try to parse a Tag11 packet from the supplied data string.
      * If the parsing was successfully, $pos will be incremented to point after the parsed data.
@@ -75,49 +75,49 @@ final class Tag11Packet extends Tag
     {
         $cur = $pos;
         $remaining = \strlen($data) - $cur;
-        
+
         if ($remaining < self::MIN_PACKET_LENGTH+2) {
             throw new ParseException('Not enough data available to read for minimum packet length.');
         }
-        
+
         if (\ord($data[$cur]) !== self::PACKET_TYPE) {
             throw new ParseException("Expected packet type marker 0x" . \bin2hex(self::PACKET_TYPE) . " but found 0x" . \bin2hex(\ord($data[$cur])));
         }
         $cur++;
-        
-        $packetSize = parent::parsePacketLength($data, $cur);
+
+        $packetSize = Util::parseTagPacketLength($data, $cur);
         if ($packetSize < self::MIN_PACKET_LENGTH) {
             throw new ParseException("Invalid packet size, minimum packet size is " . self::MIN_PACKET_LENGTH . " but got " . $packetSize);
         }
-        
+
         $remaining -= ($cur - $pos);
         if ($remaining < $packetSize) {
             throw new ParseException("Corrupt packet.");
         }
-        
+
         if (\ord($data[$cur++]) !== self::FORMAT_SPECIFIER) {
             throw new ParseException('Invalid format specifier');
         }
-        
+
         $filenameLength = \ord($data[$cur++]);
         if ($filenameLength !== \strlen(self::PACKET_FILENAME)) {
             throw new ParseException("Expected filename length of " . \strlen(self::PACKET_FILENAME) . " but got " . $filenameLength);
         }
-        
+
         $filename = \substr($data, $cur, $filenameLength);
         if ($filename !== self::PACKET_FILENAME) {
             throw new ParseException('Invalid filename "' . $filename . '", expected "' . self::PACKET_FILENAME . '".');
         }
         $cur += $filenameLength;
-        
+
         if (\substr($data, $cur, 4) !== \chr(0).\chr(0).\chr(0).\chr(0)) {
             throw new ParseException('Expected file date to be zero.');
         }
         $cur += 4;
-        
+
         $tag = new self(\substr($data, $cur, ($packetSize - self::MIN_PACKET_LENGTH)));
         $cur += ($packetSize - self::MIN_PACKET_LENGTH);
-        
+
         $pos = $cur;
         return $tag;
     }
--- src/Tag3Packet.php
+++ src/Tag3Packet.php
@@ -23,89 +23,89 @@ class Tag3Packet
      * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/ecryptfs_kernel.h?h=v4.11.3#n140
      */
     const PACKET_TYPE = 0x8C;
-    
+
     /**
      * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/keystore.c?h=v4.11.3#n1455
      */
     const PACKET_VERSION = 0x04;
-    
+
     /**
      * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/keystore.c?h=v4.11.3#n1478
      */
     const S2L_IDENTIFIER = 0x03;
-    
+
     /**
      * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/keystore.c?h=v4.11.3#n1485
      */
     const HASH_MD5_IDENTIFIER = 0x01;
-    
+
     /**
      * 65536 iterations
      * @link https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git/tree/fs/ecryptfs/keystore.c?h=v4.11.3#n2392
      */
     const HASH_DEFAULT_ITERATIONS = 0x60;
-    
-    
+
+
     /**
      * @var int
      */
     public $packetSize;
-    
+
     /**
      * @var int
      */
     public $encryptedKeySize;
-    
+
     /**
      * @var int
      */
     public $version = 0x04;
-    
+
     /**
      * @var int
      */
     public $cipherCode = 0;
-    
+
     /**
      * @var int
      */
     public $stringToKeySpecifier = 0x03;
-    
+
     /**
      * @var int
      */
     public $hashIdentifier = 0;
-    
+
     /**
      * Salt as a hex string
      * @var string
      */
     public $salt;
-    
+
     /**
      * @var int
      */
     public $hashIterations = 0;
-    
+
     /**
      * Encrypted key as hex string
      * @var string
      */
     public $encryptedKey;
-    
-    
+
+
     public function __construct(string $encryptedKey, int $cipherType = ECRYPTFS_DEFAULT_CIPHER)
     {
         $this->encryptedKey = $encryptedKey;
         $this->cipherCode = $cipherType;
     }
-    
-    
+
+
     public function generate() : string
     {
         return
               \chr(Tag3Packet::PACKET_TYPE)
-            . \chr(\strlen($this->encryptedKey) + ECRYPTFS_SALT_SIZE + 5)
+            . Util::generateTagPacketLength(\strlen($this->encryptedKey) + ECRYPTFS_SALT_SIZE + 5)
             . \chr(Tag3Packet::PACKET_VERSION)
             . \chr($this->cipherCode)
             . \chr(Tag3Packet::S2L_IDENTIFIER)
@@ -115,63 +115,63 @@ class Tag3Packet
             . $this->encryptedKey
         ;
     }
-    
-    
+
+
     /**
      * Try to parse a Tag3 packet from the supplied data string.
      * If the parsing was successfully, $pos will be incremented to point after the parsed data.
-     * 
+     *
      * Only encryptedKey and cipherCode are used, all other fields are not used.
      */
     public static function parse(string $data, int &$pos = 0) : self
     {
         $cur = $pos;
-        
+
         if (\ord($data[$cur]) !== self::PACKET_TYPE) {
             throw new ParseException("Expected packet type marker 0x" . \dechex(self::PACKET_TYPE) . " but found 0x" . \bin2hex($data[$cur]));
         }
         $cur++;
-        
-        $packetSize = Tag::parsePacketLength($data, $cur);
+
+        $packetSize = Util::parseTagPacketLength($data, $cur);
         if ($packetSize < ECRYPTFS_SALT_SIZE + 5) {
             throw new ParseException('Body size too small');
         }
-        
+
         $encryptedKeySize = $packetSize - ECRYPTFS_SALT_SIZE - 5;
         if ($encryptedKeySize > ECRYPTFS_MAX_ENCRYPTED_KEY_BYTES) {
             throw new ParseException('Expected key size too large');
         }
-        
+
         $version = \ord($data[$cur++]);
         if ($version !== self::PACKET_VERSION) {
             throw new ParseException('Invalid version number 0x' . \dechex($version));
         }
-        
+
         $cipherCode = \ord($data[$cur++]);
-        if (!isset(RFC2440_CIPHER_CODE_TO_STRING_MAPPING[$cipherCode])) {
+        if (!\array_key_exists($cipherCode, RFC2440_CIPHER_CODE_TO_STRING_MAPPING)) {
             throw new ParseException('Invalid cipher code 0x' . \dechex($cipherCode));
         }
-        
+
         $stringToKeySpecifier = \ord($data[$cur++]);
         if ($stringToKeySpecifier !== self::S2L_IDENTIFIER) {
             throw new ParseException('Only S2K ID 3 is currently supported');
         }
-        
+
         $hashIdentifier = \ord($data[$cur++]);
         if ($hashIdentifier !== self::HASH_MD5_IDENTIFIER) {
             throw new ParseException('Only MD5 as hashing algorithm supported here');
         }
-        
+
         $salt = \bin2hex(\substr($data, $cur, ECRYPTFS_SALT_SIZE));
         $cur += ECRYPTFS_SALT_SIZE;
-        
+
         /* This conversion was taken straight from RFC2440 */
-	$hashIterations = (16 + (\ord($data[$cur]) & 15)) << ((\ord($data[$cur]) >> 4) + 6);
+	    $hashIterations = (16 + (\ord($data[$cur]) & 15)) << ((\ord($data[$cur]) >> 4) + 6);
         $cur++;
-        
+
         $encryptedKey = \bin2hex(\substr($data, $cur, $encryptedKeySize));
         $cur += $encryptedKeySize;
-        
+
         $tag = new self($encryptedKey, $cipherCode);
         $pos = $cur;
         return $tag;
