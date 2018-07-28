da7e6cb3f0f77d33889bd52ee8e33c2051975351
Dennis Birkholz <dennis@birkholz.org>
2018-07-28 11:40:23 +0200

    Ecryptfs: added default cipher

--- src/constants.php
+++ src/constants.php
@@ -74,3 +74,5 @@ const ECRYPTFS_SIG_SIZE = 8;
  * @link http://bazaar.launchpad.net/~ecryptfs/ecryptfs/trunk/view/head:/src/libecryptfs/main.c#L214
  */
 const ECRYPTFS_SALT_SIZE = 8;
+
+const ECRYPTFS_DEFAULT_CIPHER_CODE = RFC2440_CIPHER_AES_256;
